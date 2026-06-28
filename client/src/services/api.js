9914d2977da8495344fdbfce9ef0b00d7a442a56
krataratha <kratarathtatran@icloud.com>
2026-06-28 17:30:27 +0530

    Update API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api",});
+    baseURL: "https://mern-task-tracker-2ekl.onrender.com/api",});
 
     export default API;
